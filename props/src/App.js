d259b2814a6a312a0d23bc853b76575bc9c32d79
sahartn <saharfarhat488@gmail.com>
2020-06-12 14:01:45 +0200

    seconde

--- props/src/App.js
+++ props/src/App.js
@@ -9,7 +9,7 @@ function App(){
     const HandleName =() =>{alert (FullName)}
     return (
         <div className='App'>
-            <Profile fullName={FullName} pofession='web' bio='A web developer is a programmer who specializes in, or is specifically engaged in, the development of World Wide Web applications using a client–server model. The applications typically use HTML, CSS and JavaScript in the client, PHP, ASP.NET (C#) or Java in the server, and http for communications between client and server. A web content management system is often used to develop and maintain web applications.
+            <Profile fullName={FullName} profession='web' bio='A web developer is a programmer who specializes in, or is specifically engaged in, the development of World Wide Web applications using a client–server model. The applications typically use HTML, CSS and JavaScript in the client, PHP, ASP.NET (C#) or Java in the server, and http for communications between client and server. A web content management system is often used to develop and maintain web applications.
 
 ' HandelName={HandleName}>
 <img src="./public/img.PNG" stlyle={{width:'200px', height:'200px'}} />       
--- props/src/index.js
+++ props/src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import Profile from './profile/Profile';
+import App from './App.js';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Profile />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
